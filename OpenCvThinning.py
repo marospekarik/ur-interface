28b38a21e6c6f3f738fc0c921ab851af4237aca8
maros.pekarik@pm.me <h8WX-}H9*Us:#4})})>
2021-11-04 20:21:12 +0100

    homography + cam + bg substraction

--- OpenCvThinning.py
+++ OpenCvThinning.py
@@ -1,80 +1,201 @@
 import math, numpy as np
 import cv2
+import Utils
+import json
+
 # READ THIS! Don't forget to install contrib version of openCV:
 # pip install opencv-contrib-python
 
-# def auto_canny(image, sigma=0.33):
-# 	# compute the median of the single channel pixel intensities
-# 	v = np.median(image)
-# 	# apply automatic Canny edge detection using the computed median
-# 	lower = int(max(0, (1.0 - sigma) * v))
-# 	upper = int(min(255, (1.0 + sigma) * v))
-# 	edged = cv2.Canny(image, lower, upper)
-# 	# return the edged image
-# 	return edged
+def GetCurrentFrame():
+	cv2.namedWindow("preview")
+	vc = cv2.VideoCapture(1)
+	i = 0
+	if vc.isOpened(): # try to get the first frame
+		rval, frame = vc.read()
+	else:
+		rval = False
 
-image = cv2.imread("abs.png")
-print(image.shape, image.dtype, image.min(), image.max(), image.mean(), image.std(), image.sum())
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-thinned = cv2.ximgproc.thinning(gray, cv2.ximgproc.THINNING_GUOHALL)
+	while rval:
+		i+=1
+		# cv2.imshow("preview", frame)
+		rval, frame = vc.read()
+		if i == 10:
+			# cv2.imshow("cap", frame)
+			# cv2.waitKey(0)
+			break
+		key = cv2.waitKey(20)
+		if key == 27: # exit on ESC
+			break
 
-#  OLD COUNTOUR EXTRACTION DOWN THERE
-cnts, _ = cv2.findContours(thinned, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-lstcont = []
-approx = []
-dist_thresh = 0
-ep_val= 0
+	# cv2.destroyWindow("preview")
+	# cv2.destroyWindow("cap")
+	cv2.imshow("frame", frame)
+	cv2.waitKey(0)
+	return frame
 
-for c in cnts:
-	# Simplify
-	epsilon = ep_val * cv2.arcLength(c, False)
-	approx.append(cv2.approxPolyDP(c, epsilon, False))
 
-for c in approx:
-	isEmpty = True
-	cont = []
-	pv = c[0][0]
-	for p in c:
-		point = np.array([p[0, 0], p[0, 1]])
-		# print point
-		if math.sqrt((pv[0] - point[0]) * (pv[0] - point[0]) + (pv[1] - point[1]) * (
-			pv[1] - point[1])) >= dist_thresh:
-			cont.append(point)
-			isEmpty = False
-			pv = point
-	if isEmpty == False:
-		lstcont.append(cont)
+def CalibrateHomography():
+	im_src = GetCurrentFrame()
+	[h, w, t] = im_src.shape
+    # Destination image
+	size = (w,h,t)
+	im_dst = np.zeros(size, np.uint8)
+	pts_dst = np.array(
+					[
+					[0,0],
+					[size[0] - 1, 0],
+					[size[0] - 1, size[1] -1],
+					[0, size[1] - 1 ]
+					], dtype=float
+					)
 
-temp_img = cv2.cvtColor(thinned, cv2.COLOR_GRAY2BGR)
-c_i = 0
-cnt_index = 0
-for c in lstcont:
-	cnt_index += 1
-	# print(lstcont)
-	# print('____________________________________________________________')
-	# print(c)
-	# print('____________________________________________________________')
-	# pv = c
-	isFirst = True
-	for p in c:
-		cn = (p[0], p[1])
-		# print(cn)
-		if isFirst == True:
+	# Show image and wait for 4 clicks.
+	cv2.imshow("Calibrate LeftTop to Right", im_src)
+	pts_src = Utils.get_four_points(im_src)
+	print("Homography Points:", pts_src)
+	# Calculate the homography
+	h, status = cv2.findHomography(pts_src, pts_dst)
+
+	# Warp source image to destination
+	im_dst = cv2.warpPerspective(im_src, h, size[0:2])
+
+	# Show output
+	cv2.imshow("Image", im_dst)
+	# cv2.waitKey(0)
+	key = cv2.waitKey(0)
+	if key == 27: # exit on ESC
+		return
+	if key == 13: # save on 'enter'
+		cv2.imwrite("background.jpg", im_dst)
+		with open('./data/homography.json', 'w') as outfile:
+			json.dump({'points': pts_src.tolist()}, outfile)
+
+def GetCurrentPerspectiveFrame():
+	im_src = GetCurrentFrame()
+	pts_src = 0
+	# Maybe rework to local state
+	with open('./data/homography.json') as json_file:
+		homography = json.load(json_file)
+		pts_src = np.array(homography.get('points'))
+		print(pts_src)
+	[h, w, t] = im_src.shape
+
+	size = (w,h,t)
+	im_dst = np.zeros(size, np.uint8)
+	pts_dst = np.array(
+					[
+					[0,0],
+					[size[0] - 1, 0],
+					[size[0] - 1, size[1] -1],
+					[0, size[1] - 1 ]
+					], dtype=float
+					)
+	h, status = cv2.findHomography(pts_src, pts_dst)
+	im_dst = cv2.warpPerspective(im_src, h, size[0:2])
+	cv2.imshow("im_dst", im_dst)
+	cv2.waitKey(0)
+	return im_dst
+
+
+def BackgroundSubtraction():
+	frame = GetCurrentPerspectiveFrame()
+	frameBg = cv2.imread("background.jpg")
+
+	first_gray = cv2.cvtColor(frameBg, cv2.COLOR_BGR2GRAY)
+	first_gray = cv2.GaussianBlur(first_gray, (21, 21), 0)
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+	difference = cv2.absdiff(gray, first_gray)
+
+	# Apply thresholding to eliminate noise
+	thresh  = cv2.threshold(difference, 40, 255, cv2.THRESH_BINARY)[1]
+	thresh = cv2.erode(thresh, None, iterations=2)
+	thresh = cv2.dilate(thresh, None, iterations=2)
+
+
+	# Transfer the thresholded image to the original image
+
+	cv2.imshow("first_gray", first_gray)
+	cv2.waitKey(0)
+	cv2.imshow("gray", gray)
+	cv2.waitKey(0)
+	cv2.imshow("Thresh", thresh)
+
+	cv2.waitKey(0)
+	return thresh
+
+
+def Thinning(image):
+	# image = cv2.imread(img)
+	# bg substraction
+	# gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+	dst = cv2.GaussianBlur(image,(5,5),cv2.BORDER_DEFAULT)
+	# inverted = cv2.bitwise_not(dst)
+	thinned = cv2.ximgproc.thinning(dst, cv2.ximgproc.THINNING_GUOHALL)
+
+	#  OLD COUNTOUR EXTRACTION DOWN THERE
+	cnts, _ = cv2.findContours(thinned, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	lstcont = []
+	approx = []
+	dist_thresh = 0
+	ep_val= 0
+
+	for c in cnts:
+		# Simplify
+		epsilon = ep_val * cv2.arcLength(c, False)
+		approx.append(cv2.approxPolyDP(c, epsilon, False))
+
+	for c in approx:
+		isEmpty = True
+		cont = []
+		pv = c[0][0]
+		for p in c:
+			point = np.array([p[0, 0], p[0, 1]])
+			# print point
+			if math.sqrt((pv[0] - point[0]) * (pv[0] - point[0]) + (pv[1] - point[1]) * (
+				pv[1] - point[1])) >= dist_thresh:
+				cont.append(point)
+				isEmpty = False
+				pv = point
+		if isEmpty == False:
+			lstcont.append(cont)
+
+	temp_img = cv2.cvtColor(thinned, cv2.COLOR_GRAY2BGR)
+	c_i = 0
+	cnt_index = 0
+	for c in lstcont:
+		cnt_index += 1
+		# print(lstcont)
+		# print('____________________________________________________________')
+		# print(c)
+		# print('____________________________________________________________')
+		# pv = c
+		isFirst = True
+		for p in c:
+			cn = (p[0], p[1])
+			# print(cn)
+			if isFirst == True:
+				pv = cn
+				isFirst = False
+			# if euclidean_dist(pv, cn) > dist_thresh:
+			if (c_i == 0):
+				cv2.line(temp_img, pv, cn, color=(255, 0, 0), thickness=1)
+			elif (c_i == 1):
+				cv2.line(temp_img, pv, cn, color=(0, 255, 0), thickness=1)
+			else:
+				cv2.line(temp_img, pv, cn, color=(0, 0, 255), thickness=1)
 			pv = cn
-			isFirst = False
-		# if euclidean_dist(pv, cn) > dist_thresh:
-		if (c_i == 0):
-			cv2.line(temp_img, pv, cn, color=(255, 0, 0), thickness=1)
-		elif (c_i == 1):
-			cv2.line(temp_img, pv, cn, color=(0, 255, 0), thickness=1)
-		else:
-			cv2.line(temp_img, pv, cn, color=(0, 0, 255), thickness=1)
-		pv = cn
 
-		c_i += 1
-		if c_i >= 3:
-			c_i = 0
+			c_i += 1
+			if c_i >= 3:
+				c_i = 0
 
-print (' contour count ', cnt_index)
-cv2.imshow("Contoured", temp_img)
-cv2.waitKey(0)
+	print (' contour count ', cnt_index)
+	cv2.imshow("Contoured", temp_img)
+	cv2.waitKey(0)
+
+	return [lstcont, temp_img]
+
+Thinning(BackgroundSubtraction())
--- Utils.py
+++ Utils.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 def figureOrientation(coord,canvasW, canvasH,rotation, mirrorW, mirrorH):
     # Mirror horizontal
     # Mirror vertical
@@ -14,8 +17,8 @@ def figureOrientation(coord,canvasW, canvasH,rotation, mirrorW, mirrorH):
     if rotation == 1:
         y = canvasH - Xprim*canvasH/canvasW
         x = Yprim*canvasW/canvasH
-    
-    # 
+
+    #
     if rotation == 2:
         y = canvasH - Yprim
         x = canvasW - Xprim
@@ -32,8 +35,33 @@ def figureOrientation(coord,canvasW, canvasH,rotation, mirrorW, mirrorH):
     # y =  -int(val[0]) * (self.canvasH/self.canvasW) + self.canvasH
     # x = int(x)
     # y = int(y)
-    
+
 
     x = int(x)
     y = int(y)
     return [x, y]
+
+def mouse_handler(event, x, y, flags, data) :
+
+    if event == cv2.EVENT_LBUTTONDOWN :
+        cv2.circle(data['im'], (x,y),3, (0,0,255), 5, 16);
+        cv2.imshow("Image", data['im']);
+        if len(data['points']) < 4 :
+            data['points'].append([x,y])
+
+def get_four_points(im):
+
+    # Set up data to send to mouse handler
+    data = {}
+    data['im'] = im.copy()
+    data['points'] = []
+
+    #Set the callback function for any mouse event
+    cv2.imshow("Image",im)
+    cv2.setMouseCallback("Image", mouse_handler, data)
+    cv2.waitKey(0)
+
+    # Convert array to np.array
+    points = np.vstack(data['points']).astype(float)
+
+    return points
